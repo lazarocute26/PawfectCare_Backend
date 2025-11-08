3cfd8834f996bd87c0b9e765f27a37f127973740
josh-lazaro26 <joshualazaro262@gmail.com>
2025-11-09 04:12:17 +0800

    testing base url

--- config/API_BASE_URL.js
+++ config/API_BASE_URL.js
@@ -1,6 +1,5 @@
 export const getApiBaseUrl = () => {
-  const host = window.location.hostname;
   return host === "localhost"
     ? "http://localhost:8081"
-    : `https://pawfectcare-ua8k.onrender.com`;
+    : "https://pawfectcare-ua8k.onrender.com";
 };
